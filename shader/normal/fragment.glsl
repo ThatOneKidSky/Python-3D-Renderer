82341420d69528632dae6e68d4f9b9d270e8ea5c
ThatOneKidSky <devonhundley1@gmail.com>
2025-02-10 23:06:33 -0500

    Fixed frag for main

--- shader/normal/fragment.glsl
+++ shader/normal/fragment.glsl
@@ -1,15 +1,15 @@
 #version 330 core
 
-in vec3 fragColor;
+in vec3 normal;
 
 out vec4 color;
 
 void main() {
-    float gNorm = max(max(abs(fragColor.x), abs(fragColor.y)), abs(fragColor.z));
+    float gNorm = max(max(abs(normal.x), abs(normal.y)), abs(normal.z));
 
-    if (gNorm == abs(fragColor.x)) {
+    if (gNorm == abs(normal.x)) {
         color = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if (gNorm == abs(fragColor.y)) {
+    } else if (gNorm == abs(normal.y)) {
         color = vec4(0.0, 1.0, 0.0, 1.0);
     } else {
         color = vec4(0.0, 0.0, 1.0, 1.0);
